1714fd96ba7887e45da0dfe47768b786558932aa
iraritchiemeek <iraritchiemeek@gmail.com>
2015-06-15 17:40:53 +1200

    Got links cleaned

--- reddit.js
+++ reddit.js
@@ -1,12 +1,13 @@
 function Reddit() {
 	this.object = []
-	this.links = []
+	this.badLinks = []
+	this.goodLinks = []
 }
 
 Reddit.prototype.getObj = function(done) {
 	$.ajax({
       type: "GET",
-      url: "https://www.reddit.com/r/OldSchoolCool/top.json"
+      url: "http://www.reddit.com/r/45thworldproblems/top.json?sort=top&t=all&limit=30"
     })
     .done(function(res) {
     	done(res)
@@ -16,7 +17,9 @@ Reddit.prototype.getObj = function(done) {
 Reddit.prototype.makeArray = function(object) {
 	var self = this
 	$.each(object.data.children, function(index, value){
-		self.object.push(value.data.url)
+		if (value.data.url) {
+			self.object.push(value.data.url)
+		}
 	})
 	return this.object
 };
@@ -24,7 +27,24 @@ Reddit.prototype.makeArray = function(object) {
 Reddit.prototype.randLinks = function(array) {
 	var self = this
 	for (var i = 0; i < 7; i++) {
-		self.links.push(array[randNum(array)])
+		self.badLinks.push(array[randNum(array)])
 	}
-	return this.links
+	return (this.cleanseLinks(this.badLinks, array))
+};
+
+Reddit.prototype.cleanseLinks = function(links, array) {
+	console.log(links)
+	var self = this
+	$.each(links, function(index, link){
+		if (link.includes("imgur") && !link.includes(".jpg") && !link.includes(".gif") && !link.includes(".png")){
+			self.goodLinks.push(link + ".gif")
+		} else if (link.includes("imgur") && link.includes(".jpg") || link.includes(".gif") || link.includes(".png")) {
+			self.goodLinks.push(link)
+		}
+	})
+	if (this.goodLinks.length === links.length) {
+		return this.goodLinks
+	} else {
+		this.randLinks(array)
+	}
 };
